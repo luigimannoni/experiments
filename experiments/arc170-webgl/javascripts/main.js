cd8fd5c1ed281a3016bd736184f9f5a2effacf48
luigimannoni <luigimannoni@gmail.com>
2015-01-07 22:08:56 +0000

    ARC Spaceship fix

--- experiments/arc170-webgl/javascripts/main.js
+++ experiments/arc170-webgl/javascripts/main.js
@@ -2,8 +2,8 @@ var mouseX, mouseY, arcSpaceship;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-renderer.setClearColor( 0x222222, 0 ); // background
 var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor( 0x222222, 0 ); // background
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
