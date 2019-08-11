d122b46e233349775bac028b4c81fac12eb6bfcc
Luigi Mannoni <luigimannoni@gmail.com>
2019-08-11 21:49:37 +0100

    Load skybox

--- src/components/Three/Experiments/Arc170/index.jsx
+++ src/components/Three/Experiments/Arc170/index.jsx
@@ -20,10 +20,10 @@ export default class Arc170 extends Base {
       75,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000,
+      5000,
     );
 
-    camera.position.x = -220;
+    camera.position.x = -1500;
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setClearColor(0x222222, 0);
 
@@ -32,54 +32,79 @@ export default class Arc170 extends Base {
 
     const controls = new TrackballControls(camera, this.renderer.domElement);
     controls.target = scene.position;
-    controls.minDistance = 50;
-    controls.maxDistance = 300;
+    controls.minDistance = 800;
+    controls.maxDistance = 2000;
 
-    // Background
-    const background = new THREE.Mesh(
-      new THREE.SphereGeometry(312, 32, 32),
-      new THREE.MeshStandardMaterial({
-        color: 0x0,
-        side: THREE.BackSide,
-      }),
+    // Skybox
+    const r = '/assets/skyboxes/nebula1024_';
+    const urls = [
+      `${r}right1.png`, `${r}left2.png`,
+      `${r}top3.png`, `${r}bottom4.png`,
+      `${r}front5.png`, `${r}back6.png`,
+    ];
+
+    const textureCube = new THREE.CubeTextureLoader().load(urls);
+    textureCube.format = THREE.RGBFormat;
+    textureCube.mapping = THREE.CubeReflectionMapping;
+    textureCube.encoding = THREE.sRGBEncoding;
+
+    const cubeShader = THREE.ShaderLib.cube;
+    const skyboxMaterial = new THREE.ShaderMaterial({
+      fragmentShader: cubeShader.fragmentShader,
+      vertexShader: cubeShader.vertexShader,
+      uniforms: cubeShader.uniforms,
+      depthWrite: false,
+      side: THREE.BackSide,
+    });
+
+    skyboxMaterial.uniforms.tCube.value = textureCube;
+    Object.defineProperty(skyboxMaterial, 'map', {
+      get() {
+        return this.uniforms.tCube.value;
+      },
+    });
+
+    const skybox = new THREE.Mesh(
+      new THREE.BoxBufferGeometry(5000, 5000, 5000),
+      skyboxMaterial,
     );
-    scene.add(background);
+    scene.add(skybox);
 
     // Lights
     const light = new THREE.AmbientLight(0xffffff, 2.0);
     scene.add(light);
 
     const l1 = new THREE.PointLight(0xffffff, 0.5);
-    l1.position.set(0, -200, -200);
+    l1.position.set(0, -2000, -2000);
     scene.add(l1);
 
     const l2 = new THREE.PointLight(0xffffff, 1.5);
-    l2.position.set(0, 200, 200);
+    l2.position.set(0, 2000, 2000);
     scene.add(l2);
 
-
-    // Mesh
+    let arc170 = null;
+    // Model Mesh
     const loader = new GLTFLoader();
     loader.load(`${process.env.PUBLIC_URL}/assets/arc170/scene.gltf`, (gltf) => {
-      const model = gltf;
-
-      model.scene.scale.x = 0.15;
-      model.scene.scale.y = 0.15;
-      model.scene.scale.z = 0.15;
-      scene.add(model.scene);
+      [arc170] = gltf.scene.children;
+      scene.add(arc170);
     });
 
     const render = () => {
       super.beforeRender();
       const time = performance.now() / 2000;
 
-      l1.position.x = Math.sin(Math.sin(time)) * 250;
-      l1.position.y = Math.sin(Math.cos(time)) * 250;
-      l1.position.z = Math.sin(time) * 250;
+      l1.position.x = Math.sin(Math.sin(time)) * 2000;
+      l1.position.y = Math.sin(Math.cos(time)) * 2000;
+      l1.position.z = Math.sin(time) * 2000;
 
-      l2.position.x = Math.sin(time) * -250;
-      l2.position.y = Math.cos(time) * -250;
-      l2.position.z = Math.cos(Math.sin(time)) * -250;
+      l2.position.x = Math.sin(time) * -2000;
+      l2.position.y = Math.cos(time) * -2000;
+      l2.position.z = Math.cos(Math.sin(time)) * -2000;
+
+      if (arc170 && arc170.rotation) {
+        arc170.rotation.y = Math.cos(time) / 2;
+      }
 
       controls.update();
       this.renderer.render(scene, camera);
